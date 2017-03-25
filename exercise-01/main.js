160c0cc74d9fa884f67ee9ac43bbc4bfaef55da0
robsonfagundes <robsonfagundes@gmail.com>
2017-03-25 11:45:13 -0300

    Exercise One

---
+++ exercise-01/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let sql = 'CREATE TABLE author(id NUMBER, name STRING, city STRING, state STRING, country STRING)';
+
+let tableName = sql.match(/([a-zA-Z]+)/g)[1];
+let columns = sql.match(/([a-z\s,])/g)[2];
+
+console.log(tableName);
+console.log(columns);
